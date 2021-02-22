187533c659792c9c8a4de072bcd4fdf6bf3af111
Haidar A <haidarzsh@gmail.com>
2021-02-22 23:08:16 +0500

    Changed border colors and top bar colors

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -18,10 +18,12 @@ static const char col_gray3[]       = "#bdae93";
 static const char col_gray4[]       = "#ebdbb2";
 static const char col_cyan[]        = "#076678";
 static const char col_yellow[]	    = "#d79921";
+static const char col_brown[]	    = "#504945";
+static const char col_red[]	    = "#cc241d";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_yellow },
+	[SchemeSel]  = { col_gray4, col_brown, col_red },
 };
 
 /* tagging */
@@ -67,7 +69,7 @@ static const char *termcmd[]  = { "st", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
