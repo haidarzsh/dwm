c17c0f759daaa3f4b6905e6cd7659ca3b9f0ffdc
Haidar Ali <haidarzsh@gmail.com>
2021-03-19 17:00:54 +0500

    Added switchcol patchclea

--- config.h
+++ config.h
@@ -49,10 +49,11 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]",       tile },    /* first entry is default */
+	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "[G]",      grid },
+	{ "|||",      tcl },
 	{ NULL,	      NULL},
 };
 
@@ -89,6 +90,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,			XK_g,	   setlayout,	   {.v = &layouts[3]} },
+	{ MODKEY,			XK_c,	   setlayout,	   {.v = &layouts[4]} },
+	{ MODKEY, 			XK_n,	   switchcol,	   {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
